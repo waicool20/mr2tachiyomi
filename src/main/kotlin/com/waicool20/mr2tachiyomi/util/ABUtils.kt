1b535f6cf262fcd67029ea5ea6ee2198f1daf9f2
waicool20 <waicool20@gmail.com>
2019-09-09 21:06:57 +0800

    Optimized memory footprint for ab2tar implementation
    Cuts and corrects the file as its read then writes back to disk immediately instead of saving the cut result in an intermediate bytearray then doing a second pass for header correction

--- src/main/kotlin/com/waicool20/mr2tachiyomi/util/ABUtils.kt
+++ src/main/kotlin/com/waicool20/mr2tachiyomi/util/ABUtils.kt
@@ -20,13 +20,12 @@
 package com.waicool20.mr2tachiyomi.util
 
 import org.slf4j.LoggerFactory
-import java.io.ByteArrayOutputStream
 import java.io.InputStream
+import java.io.OutputStream
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.file.Files
 import java.nio.file.Path
-import java.nio.file.StandardOpenOption
 import kotlin.math.min
 
 object ABUtils {
@@ -34,19 +33,19 @@ object ABUtils {
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    fun ab2tar(input: Path): ByteArray {
-        logger.debug("Android Backup to Tar Archive, input file: ${input.toAbsolutePath()}")
-        return correctFile(cutFile(Files.newInputStream(input)))
-    }
-
     fun ab2tar(input: Path, output: Path) {
-        Files.write(output, ab2tar(input), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
+        logger.debug("Android Backup to Tar Archive, input file: ${input.toAbsolutePath()}")
+        Files.newInputStream(input).use { i ->
+            Files.newOutputStream(output).use { o ->
+                processFile(i, o)
+            }
+        }
     }
 
     /**
      * Original C implementation over here: https://github.com/floe/helium_ab2tar/blob/master/ab2tar_cut.c
      */
-    private fun cutFile(input: InputStream): ByteArray {
+    private fun processFile(input: InputStream, output: OutputStream) {
         val byteBuffer = ByteBuffer.allocate(65536).order(ByteOrder.LITTLE_ENDIAN)
 
         byteBuffer.putInputStreamAndFlip(input, 26)
@@ -63,23 +62,40 @@ object ABUtils {
         var size: Int
         var compl: Int
 
-        val cutFile = ByteArrayOutputStream()
         val blockDebug = StringBuilder("blocks? ")
+        var nextOffset = 0
+
+        val blockDataBuffer = ByteBuffer.allocate(5).order(ByteOrder.LITTLE_ENDIAN)
+        byteBuffer.clear()
         while (type == 0 && input.available() > 0) {
-            byteBuffer.clear()
-            byteBuffer.putInputStreamAndFlip(input, 5)
-            type = byteBuffer.get().toInt()
-            size = byteBuffer.short.toUShort().toInt()
-            compl = byteBuffer.short.toUShort().toInt()
+            blockDataBuffer.clear()
+            blockDataBuffer.putInputStreamAndFlip(input, 5)
+            type = blockDataBuffer.get().toInt()
+            size = blockDataBuffer.short.toUShort().toInt()
+            compl = blockDataBuffer.short.toUShort().toInt()
 
             if (size + compl != 65535) blockDebug.append("@")
             blockDebug.append("%02x (0x%04x) ".format(type, size))
 
             while (size > 0) {
-                byteBuffer.clear()
-                val bytes = min(byteBuffer.capacity(), size)
+                val bytes = min(byteBuffer.remaining(), size)
                 byteBuffer.putInputStreamAndFlip(input, bytes)
-                cutFile.write(byteBuffer.array(), 0, byteBuffer.limit())
+                logger.debug("---- Next offset: $nextOffset")
+                if (nextOffset > bytes) {
+                    nextOffset -= bytes
+                } else {
+                    nextOffset = correctHeaders(byteBuffer, nextOffset) - bytes
+                }
+
+                val copyBytes = if (nextOffset < 0) {
+                    (bytes + nextOffset).also { nextOffset = 0 }
+                } else {
+                    bytes
+                }
+
+                output.write(byteBuffer.array(), 0, copyBytes)
+                byteBuffer.position(byteBuffer.position() + copyBytes)
+                byteBuffer.compact()
                 size -= bytes
             }
         }
@@ -87,42 +103,59 @@ object ABUtils {
 
         byteBuffer.clear()
         byteBuffer.putInputStreamAndFlip(input, 4)
-        logger.debug("chksum? %02x %02x %02x %02x".format(byteBuffer.get(), byteBuffer.get(), byteBuffer.get(), byteBuffer.get()))
+        logger.debug(
+            "chksum? %02x %02x %02x %02x".format(
+                byteBuffer.get(),
+                byteBuffer.get(),
+                byteBuffer.get(),
+                byteBuffer.get()
+            )
+        )
 
-        return cutFile.toByteArray()
+        input.close()
+        output.close()
     }
 
     /**
      * Original C implementation over here: https://github.com/floe/helium_ab2tar/blob/master/tar2ab_corr.c
      */
-    private fun correctFile(input: ByteArray): ByteArray {
-        val buffer = ByteArray(513)
-        val inputBytes = ByteBuffer.wrap(input).order(ByteOrder.LITTLE_ENDIAN)
+    private fun correctHeaders(input: ByteBuffer, startOffset: Int): Int {
+        val byteBuffer = input.duplicate()
+        var offset = startOffset
+        while ((offset + 512) < byteBuffer.limit()) {
+            byteBuffer.position(offset)
+            logger.debug("File: ${byteBuffer.getString(TarHeaderOffsets.NAME_RANGE.last, true)}")
 
-        val corrFile = ByteArrayOutputStream()
-        while (inputBytes.hasRemaining()) {
-            inputBytes.get(buffer, 0, 512)
-            // Correct size and time in header
-            val numberOfBlocks = if (buffer[0] > 0) {
-                val size = String(buffer, TarHeaderOffsets.SIZE + 1, 11).toLong(8)
-                val time = String(buffer, TarHeaderOffsets.MTIME + 1, 11).toLong(8)
-                buffer[TarHeaderOffsets.SIZE + 11] = 0
-                buffer[TarHeaderOffsets.MTIME + 11] = 0
-                String.format("%011o", size).toByteArray().copyInto(buffer, TarHeaderOffsets.SIZE)
-                String.format("%011o", time).toByteArray().copyInto(buffer, TarHeaderOffsets.MTIME)
+            val numberOfBlocks = if (byteBuffer[offset] > 0) {
+                byteBuffer.position(offset + TarHeaderOffsets.SIZE + 1)
+                val size = byteBuffer.getString(11).toLong(8)
+                logger.debug("Size: $size | Offset: $offset | Buffer limit: ${byteBuffer.limit()}")
+                byteBuffer.position(offset + TarHeaderOffsets.MTIME + 1)
+                val time = byteBuffer.getString(11).toLong(8)
+                byteBuffer.put(offset + TarHeaderOffsets.SIZE + 11, 0)
+                byteBuffer.put(offset + TarHeaderOffsets.MTIME + 11, 0)
+
+                byteBuffer.position(offset + TarHeaderOffsets.SIZE)
+                byteBuffer.put("%011o".format(size).toByteArray())
+                byteBuffer.position(offset + TarHeaderOffsets.MTIME)
+                byteBuffer.put("%011o".format(time).toByteArray())
                 if (size > 0) 1 + (size - 1) / 512 else 0
             } else 0
+            logger.debug("Blocks: $numberOfBlocks | Stored size: ${numberOfBlocks * 512}")
 
             // Adjust checksum header
-            for (i in 0 until 8) buffer[TarHeaderOffsets.CHKSUM + i] = ' '.toByte()
-            String.format("%06o\u0000 ", buffer.sliceArray(0..512).sum()).toByteArray()
-                .copyInto(buffer, TarHeaderOffsets.CHKSUM)
+            byteBuffer.position(offset + TarHeaderOffsets.CHKSUM)
+            byteBuffer.put("        ".toByteArray())
 
-            corrFile.write(buffer, 0, 512)
-            repeat(512 * numberOfBlocks.toInt()) {
-                corrFile.write(inputBytes.get().toInt())
+            var newCheckSum = 0
+            for (i in 0 until 512) {
+                newCheckSum += byteBuffer.get(offset + i)
             }
+            byteBuffer.position(offset + TarHeaderOffsets.CHKSUM)
+            byteBuffer.put("%06o\u0000 ".format(newCheckSum).toByteArray())
+
+            offset += (numberOfBlocks.toInt() + 1) * 512
         }
-        return corrFile.toByteArray()
+        return offset
     }
 }
